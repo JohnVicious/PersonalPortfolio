e763576b8c84f63bd60523c041c40e06ab540c19
JohnKlein985 <John@apexinnovations.com>
2024-02-08 15:28:05 -0600

    Folder change

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('projects')->insert([
+            'snapshot'=>'resumeSiteSnapshot.png',
+            'description'=>'My first project in my portfolio, my resume site. Built from the ground up to showcase myself, skillset, and projects.',
+            'url'=>'/',
+			'github'=>'https://github.com/JohnVicious/PersonalPortfolio',
+			'active'=>1
+        ]);
+		
+        DB::table('projects')->insert([
+            'snapshot'=>'assetViewerSnapshot.png',
+            'description'=>'An Asset Viewer that I originally made to test PHP Imagick in my spare time. Eventually adapted it to be used at my work in order to help developers and clinical staff view our custom made assets quickly and efficiently.',
+            'url'=>'/AssetViewer',
+			'github'=>null,
+			'active'=>0
+        ]);
+		
+        DB::table('projects')->insert([
+            'snapshot'=>'recipeJournalSnapshot.png',
+            'description'=>'A Vue-Native application created for my favorite recipes.',
+            'url'=>'/recipeJournal',
+			'github'=>'https://github.com/JohnVicious/RecipeJournal',
+			'active'=>1
+        ]);
+    }
+}
